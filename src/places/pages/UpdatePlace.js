176cd7dba7dd1a02542489b3c0f03fc11b97d9d8
Sanchit <sanchitbhadgal36@gmail.com>
2022-01-02 14:47:42 +0530

    env variables added

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -40,7 +40,7 @@ const UpdatePlace = () => {
     const fetchData = async () => {
       try {
         const responseData = await sendRequest(
-          `http://localhost:5000/api/places/${placeId}`
+          `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`
         );
         setPlace(responseData.place);
       } catch (error) {}
@@ -52,7 +52,7 @@ const UpdatePlace = () => {
     event.preventDefault();
     try {
       await sendRequest(
-        `http://localhost:5000/api/places/${placeId}`,
+        `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`,
         'PATCH',
         JSON.stringify({
           title: formState.inputs.title.value,
