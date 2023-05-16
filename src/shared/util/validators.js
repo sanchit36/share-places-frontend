4a9c2b730f3b61e550a1e28c12e449a2ca44f38e
Sanchit Bhadgal <sanchitbhadgal36@gmail.com>
2023-05-16 18:03:44 +0530

    validator 2 added

--- src/shared/util/validators.js
+++ src/shared/util/validators.js
@@ -44,3 +44,28 @@ export const validate = (value, validators) => {
   }
   return isValid;
 };
+
+export const validate2 = (value, validators) => {
+  let isValid = true;
+  for (const validator of validators) {
+    if (validator.type === VALIDATOR_TYPE_REQUIRE) {
+      isValid = isValid && value.trim().length > 0;
+    }
+    if (validator.type === VALIDATOR_TYPE_MINLENGTH) {
+      isValid = isValid && value.trim().length >= validator.val;
+    }
+    if (validator.type === VALIDATOR_TYPE_MAXLENGTH) {
+      isValid = isValid && value.trim().length <= validator.val;
+    }
+    if (validator.type === VALIDATOR_TYPE_MIN) {
+      isValid = isValid && +value >= validator.val;
+    }
+    if (validator.type === VALIDATOR_TYPE_MAX) {
+      isValid = isValid && +value <= validator.val;
+    }
+    if (validator.type === VALIDATOR_TYPE_EMAIL) {
+      isValid = isValid && /^\S+@\S+\.\S+$/.test(value);
+    }
+  }
+  return isValid;
+};
